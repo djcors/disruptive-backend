267892c9af0d1975234bf5848b147e742c996cc6
Jonathan Cortes <jonathan.cortes@ecosystems.solutions>
2023-07-13 09:36:04 -0500

    keys added, perform metric getter

--- src/application/modules/metrics/useCases/index.ts
+++ src/application/modules/metrics/useCases/index.ts
@@ -1,8 +1,9 @@
 import { Assets } from "../../../../adapters/providers/messari/metrics/models/Assets.enum";
+import { TryResult, TryWrapper } from "../../../../domain/shared/utils/TryWrapper";
 import { LocaleTypeEnum } from "../../../shared/locals/LocaleType.enum";
 import { UseCaseTrace } from "../../../shared/log/UseCaseTrace";
 import { ILogProvider } from "../../../shared/log/providerContracts/ILogProvider";
-import { BaseUseCase, IResultT, ResultT } from "../../../shared/useCase/BaseUseCase";
+import { BaseUseCase, IResult, IResultT, ResultT } from "../../../shared/useCase/BaseUseCase";
 
 import { MetricsDto } from "../dtos/MetricsDto";
 import { IMetricProvider } from "../providerContracts/IMetric.provider";
@@ -19,21 +20,47 @@ export class MetricUseCase extends BaseUseCase<{asset: string}> {
       ): Promise<IResultT<MetricsDto>> {
 
         this.setLocale(locale);
+        this.initializeUseCaseTrace(trace, args.asset, []);
         const result = new ResultT<MetricsDto>();
-        const message = await this.metricProvider.getMetric(
-          args.asset
-          );
-        
-        console.error(message)
-        if (!message.success) {
-          result.setError(
-            this.appMessages.get(this.appMessages.keys.INVALID_USER_OR_PASSWORD),
-            this.applicationStatus.INVALID_INPUT,
-          );
+        if (!this.validate(args.asset)) {
+          this.setError(result);
+          return result;
         }
+
+        const metricResult = await this.getMetric(result, args.asset);
+
+        if (!metricResult.success) return result;
         
-        result.setData(message, this.applicationStatus.SUCCESS);
-      
+        result.setData(metricResult.value as MetricsDto, this.applicationStatus.SUCCESS);
+
+        
         return result;
+      }
+
+      async getMetric(
+        result: IResult,
+        asset: string,
+      ): Promise<TryResult<MetricsDto>> {
+
+        const authenticatedResult = await TryWrapper.syncExec(
+          this.metricProvider.getMetric(asset)
+        );
+
+        if (!authenticatedResult.success) {
+          this.setError(result);
+        }
+
+        return authenticatedResult
+      }
+
+      validate(asset: string) {
+        return Object.values(Assets).some(coin =>  coin === asset);
+      }
+
+      setError(result: IResult) {
+        result.setError(
+          this.appMessages.get(this.appMessages.keys.INVALID_ASSET),
+          this.applicationStatus.NOT_FOUND,
+        );
       }
 }
